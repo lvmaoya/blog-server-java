8301967c9b381e3eda9c7a9bc972d1bd75219aab
sunjianxiang <1504734652@qq.com>
2025-04-30 17:06:27 +0800

    comment 优化

--- src/main/java/com/lvmaoya/blog/h5Controller/H5BlogController.java
+++ src/main/java/com/lvmaoya/blog/h5Controller/H5BlogController.java
@@ -2,13 +2,17 @@ package com.lvmaoya.blog.h5Controller;
 
 import com.lvmaoya.blog.domain.dto.BlogPostDto;
 import com.lvmaoya.blog.domain.dto.CategoryGetDto;
+import com.lvmaoya.blog.domain.dto.CommentPostDto;
 import com.lvmaoya.blog.domain.searchParams.BlogListSearchParams;
+import com.lvmaoya.blog.domain.searchParams.CommentSearchParams;
 import com.lvmaoya.blog.domain.vo.R;
 import com.lvmaoya.blog.domain.vo.UserVo;
 import com.lvmaoya.blog.service.BlogService;
 import com.lvmaoya.blog.service.CategoryService;
+import com.lvmaoya.blog.service.CommentService;
 import com.lvmaoya.blog.service.UserService;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,6 +29,9 @@ public class H5BlogController {
     private UserService userService;
     @Resource
     private CategoryService categoryService;
+
+    @Resource
+    private CommentService commentService;
     @GetMapping("/user")
     public R<UserVo> getUserDetail(){
         return R.success(userService.getUser(1));
@@ -45,4 +52,12 @@ public class H5BlogController {
         return blogService.getBlogById(id);
     }
 
+    @GetMapping("/comment/list")
+    public R getCommentList(@ModelAttribute CommentSearchParams commentSearchParams) {
+        return commentService.selectList(commentSearchParams);
+    }
+    @PostMapping("/comment")
+    public R addOrUpdateComment(@RequestBody CommentPostDto commentPostDto, HttpServletRequest request) {
+        return commentService.addOrUpdateComment(commentPostDto, request);
+    }
 }
